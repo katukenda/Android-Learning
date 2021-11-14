f8d69876ef5192334491bd3490e9c684a9b2c934
katukenda <katukenda@icloud.com>
2021-11-14 23:32:22 +0530

    notification

--- app/src/main/java/com/example/flashscreendemo/MainActivity.java
+++ app/src/main/java/com/example/flashscreendemo/MainActivity.java
@@ -1,18 +1,53 @@
 package com.example.flashscreendemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button notifiBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
+        notifiBtn = findViewById(R.id.notify_btn);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel("My Notification", "My Notification", NotificationManager.IMPORTANCE_DEFAULT);
+NotificationManager manager = getSystemService(NotificationManager.class);
+manager.createNotificationChannel(channel);
+        }
+
+        notifiBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //notification code goes here
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, "My Notification");
+          builder.setContentTitle(("My Titile"));
+          builder.setContentText("Hello world, This is a simple notification");
+          builder.setSmallIcon(R.drawable.ic_launcher_background);
+          builder.setAutoCancel(true);
+
+                NotificationManagerCompat managerCompat = NotificationManagerCompat.from(MainActivity.this);
+                managerCompat.notify(1, builder.build());
+
+
+            }
+        });
+
     }
 }
