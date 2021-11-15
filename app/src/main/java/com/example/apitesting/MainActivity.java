fed6b524c37ac3a6359af0d40dc6c9bcd2248230
katukenda <katukenda@icloud.com>
2021-11-15 13:14:36 +0530

    Checek internet connectivity

--- app/src/main/java/com/example/apitesting/MainActivity.java
+++ app/src/main/java/com/example/apitesting/MainActivity.java
@@ -2,17 +2,26 @@ package com.example.apitesting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.button.MaterialButton;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.net.NetworkInterface;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -21,6 +30,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MaterialButton checkBtn = (MaterialButton) findViewById(R.id.check);
+
+        checkBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isConnected())
+                    Toast.makeText(getApplicationContext(), "Internet Is Available", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(getApplicationContext(), "Internet Is Not Available", Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
 
         TextView tv = (TextView) findViewById(R.id.hello);
@@ -54,5 +76,31 @@ e.printStackTrace();
         requestQueue.add(jsonObjectRequest);
 
 
+
+
+
+
     }
+
+    boolean isConnected(){
+
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        if(networkInfo!=null){
+
+            if(networkInfo.isConnected())
+                return true;
+            else
+                return false;
+
+        }
+        else
+            return false;
+    }
+
+
+
+
 }
