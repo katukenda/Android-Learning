c0ccfcd6f17352f4ac711eb7bd6f8a77524f4684
katukenda <katukenda@icloud.com>
2021-11-15 06:43:56 +0530

    add side menu

--- app/src/main/java/com/example/customactionbar/MainActivity.java
+++ app/src/main/java/com/example/customactionbar/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.customactionbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,11 +30,38 @@ public class MainActivity extends AppCompatActivity {
         rightIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "You Click in right icon", Toast.LENGTH_SHORT).show();
+                showMenu(v);
             }
         });
 
         title.setText(("Easy Tutorial"));
 
     }
+
+    private void showMenu(View v){
+        PopupMenu popupMenu = new PopupMenu(MainActivity.this,v);
+        popupMenu.getMenuInflater().inflate(R.menu.popup_menu,popupMenu.getMenu());
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if(item.getItemId() == R.id.one){
+                    Toast.makeText(MainActivity.this, "You Click One ", Toast.LENGTH_SHORT).show();
+                }
+                else if(item.getItemId() == R.id.two){
+                    Toast.makeText(MainActivity.this, "You Click Two ", Toast.LENGTH_SHORT).show();
+                }
+                else if(item.getItemId() == R.id.three){
+                    Toast.makeText(MainActivity.this, "You Click Three ", Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+        });
+
+        popupMenu.show();
+
+
+    }
+
+
+
 }
